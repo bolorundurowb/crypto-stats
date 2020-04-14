c688622a93eb9ded993fc0935046ab6f17a967e9
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-14 14:10:14 +0100

    ft: add model

---
+++ coin-stats/coin-stats/Models/Data/Coin.cs
@@ -0,0 +1,17 @@
+﻿namespace coin_stats.Models.Data
+{
+    public class Coin
+    {
+        public string Id { get; set; }
+        
+        public string Rank { get; set; }
+        
+        public string Symbol { get; set; }
+        
+        public string Name { get; set; }
+
+        public decimal PriceUsd { get; set; }
+
+        public double ChangePercent24Hr { get; set; }
+    }
+}
