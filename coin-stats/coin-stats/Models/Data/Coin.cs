c344c842feb5cd352307b1659863f101d3cf7fcf
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-14 15:01:19 +0100

    ft: add in the item view structure

--- coin-stats/coin-stats/Models/Data/Coin.cs
+++ coin-stats/coin-stats/Models/Data/Coin.cs
@@ -1,4 +1,7 @@
-﻿namespace coin_stats.Models.Data
+﻿using System;
+using Xamarin.Forms;
+
+namespace coin_stats.Models.Data
 {
     public class Coin
     {
@@ -14,16 +17,40 @@
 
         public string ChangePercent24Hr { get; set; }
 
-        public decimal PriceInUsd()
+        public decimal PriceInUsd
         {
-            decimal.TryParse(PriceUsd, out var price);
-            return price;
+            get
+            {
+                decimal.TryParse(PriceUsd, out var price);
+                return Math.Round(price, 2);
+            }
         }
 
-        public double LastDayChange()
+        public double LastDayChange
         {
-            double.TryParse(ChangePercent24Hr, out var change);
-            return change;
+            get
+            {
+                double.TryParse(ChangePercent24Hr, out var change);
+                return Math.Round(change, 2);
+            }
+        }
+
+        public Color LastDayColor
+        {
+            get
+            {
+                if (LastDayChange < 0)
+                {
+                    return Color.Red;
+                }
+
+                if (LastDayChange > 0)
+                {
+                    return Color.Green;
+                }
+
+                return Color.White;
+            }
         }
     }
 }
