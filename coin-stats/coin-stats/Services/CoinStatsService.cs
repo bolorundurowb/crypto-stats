fe49cfafde65456c17564ebc678c219b87a87341
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-14 14:22:35 +0100

    ft: add the coin stats service

---
+++ coin-stats/coin-stats/Services/CoinStatsService.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using coin_stats.Models.Data;
+
+namespace coin_stats.Services
+{
+    public class CoinStatsService
+    {
+        private const string ApiUrl = "https://api.coincap.io/v2/assets";
+        private static readonly HttpClient Client = new HttpClient();
+
+        private static readonly JsonSerializerOptions DeserializerOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        public async Task<Coins> GetAllStats()
+        {
+            var response = await Client.GetStreamAsync(ApiUrl);
+            return await JsonSerializer.DeserializeAsync<Coins>(response, DeserializerOptions);
+        }
+    }
+}
