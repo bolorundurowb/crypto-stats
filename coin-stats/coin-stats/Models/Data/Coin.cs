c1de7c8bce6a799aad5d51d1d3e6b77be93f64ac
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-14 14:30:38 +0100

    ft: add reader for number values

--- coin-stats/coin-stats/Models/Data/Coin.cs
+++ coin-stats/coin-stats/Models/Data/Coin.cs
@@ -3,15 +3,27 @@
     public class Coin
     {
         public string Id { get; set; }
-        
+
         public string Rank { get; set; }
-        
+
         public string Symbol { get; set; }
-        
+
         public string Name { get; set; }
 
         public string PriceUsd { get; set; }
 
         public string ChangePercent24Hr { get; set; }
+
+        public decimal PriceInUsd()
+        {
+            decimal.TryParse(PriceUsd, out var price);
+            return price;
+        }
+
+        public double LastDayChange()
+        {
+            double.TryParse(ChangePercent24Hr, out var change);
+            return change;
+        }
     }
 }
