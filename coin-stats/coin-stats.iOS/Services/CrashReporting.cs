76a1146fd16f1a3b2d8cdd412fa7ed97d5270f6f
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-22 16:20:23 +0100

    ft: add the ios crash reporting

---
+++ coin-stats/coin-stats.iOS/Services/CrashReporting.cs
@@ -0,0 +1,38 @@
+﻿using coin_stats.Services;
+
+namespace coin_stats.iOS.Services
+{
+    public class CrashReporting : ICrashReporting
+    {
+        public Boolean CrashReportingInit()
+        {
+            try
+            {
+                Firebase.Core.App.Configure();
+
+                Crashlytics.Configure();
+                //Fabric.Fabric.SharedSdk.Debug = true;
+
+                Fabric.Fabric.With(typeof(Crashlytics));
+            }
+            catch (Exception exception)
+            {
+                System.Diagnostics.Debug.WriteLine("CrashReportingInit - " +
+                                                   " failed - " + exception.Message);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Boolean CrashReportingMisc()
+        {
+            return false;
+        }
+
+        public Void ForceCrash()
+        {
+            throw new ApplicationException("This is a forced crash - iOS");
+        }
+    }
+}
