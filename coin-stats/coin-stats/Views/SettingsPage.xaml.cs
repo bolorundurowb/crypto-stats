6620ef9a9bf567a6b6594656b713f8616ae6ef31
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-05-06 09:16:12 +0100

    ft: update the settings page

--- coin-stats/coin-stats/Views/SettingsPage.xaml.cs
+++ coin-stats/coin-stats/Views/SettingsPage.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Expandable;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace coin_stats.Views
@@ -15,27 +9,6 @@ namespace coin_stats.Views
         public SettingsPage()
         {
             InitializeComponent();
-        }
-
-        protected async void OnStatusChanged(object sender, StatusChangedEventArgs e)
-        {
-            var expandableView = sender as ExpandableView;
-            var header = expandableView?.Children
-                .OfType<Grid>()
-                .FirstOrDefault();
-            var label = header?.Children
-                .OfType<Label>()
-                .LastOrDefault();
-
-            if (label != null && e.Status == ExpandStatus.Expanding)
-            {
-                await label.RotateTo(180, 200, Easing.CubicIn);
-            }
-
-            if (label != null && e.Status == ExpandStatus.Collapsing)
-            {
-                await label.RotateTo(0, 200, Easing.CubicIn);
-            }
         }
     }
 }
