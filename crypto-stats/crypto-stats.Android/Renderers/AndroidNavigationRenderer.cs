0eb7465eefa072f11565aa63d5e4ead7259ec211
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-04-20 12:54:13 +0100

    ft: whitespace fixes

--- crypto-stats/crypto-stats.Android/Renderers/AndroidNavigationRenderer.cs
+++ crypto-stats/crypto-stats.Android/Renderers/AndroidNavigationRenderer.cs
@@ -8,36 +8,38 @@ using TextAlignment = Android.Views.TextAlignment;
 using View = Android.Views.View;
 
 [assembly: ExportRenderer(typeof(NavigationPage), typeof(AndroidNavigationRenderer))]
-
 namespace crypto_stats.Android.Renderers
 {
     public class AndroidNavigationRenderer : NavigationPageRenderer
     {
         private Toolbar _toolbar;
 
-        public AndroidNavigationRenderer(Context context) : base(context)
-        {
-        }
+        public AndroidNavigationRenderer(Context context) : base(context) { }
 
         public override void OnViewAdded(View child)
         {
             base.OnViewAdded(child);
-            if (child.GetType() == typeof(Toolbar))
+            
+            if (child.GetType() != typeof(Toolbar))
             {
-                _toolbar = (Toolbar) child;
-                _toolbar.ChildViewAdded += Toolbar_ChildViewAdded;
+                return;
             }
+            
+            _toolbar = (Toolbar) child;
+            _toolbar.ChildViewAdded += Toolbar_ChildViewAdded;
         }
 
         private void Toolbar_ChildViewAdded(object sender, ChildViewAddedEventArgs e)
         {
-            if (e.Child?.GetType() == typeof(AppCompatTextView))
+            if (e.Child?.GetType() != typeof(AppCompatTextView))
             {
-                var textView = (AppCompatTextView) e.Child;
-                textView.Typeface = Typeface.CreateFromAsset(Context?.Assets, "GorditaBold.otf");
-                textView.TextAlignment = TextAlignment.Center;
-                _toolbar.ChildViewAdded -= Toolbar_ChildViewAdded;
+                return;
             }
+            
+            var textView = (AppCompatTextView) e.Child;
+            textView.Typeface = Typeface.CreateFromAsset(Context?.Assets, "GorditaBold.otf");
+            textView.TextAlignment = TextAlignment.Center;
+            _toolbar.ChildViewAdded -= Toolbar_ChildViewAdded;
         }
     }
 }
